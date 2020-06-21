1350be9562f1bd0d9b8cb9673fc9c62d706b1364
Felipe Palma <felipepalmacastro@gmail.com>
2020-06-20 22:09:02 -0400

    Recuperandome

--- receptor/src/index.js
+++ receptor/src/index.js
@@ -1,10 +1,15 @@
 let express = require('express')
 let app = express()
 
+let rutaProducto = require('./rutas/producto')
+
+app.use(express.static('public'))
+app.use(rutaProducto)
+
 console.log("Receptor Operativo. Saludos")
 
 app.use(express.static('public'))
 
 const PUERTO = process.env.PORT || 3000
 
-app.listen(3000)
+app.listen(PUERTO, () => console.info(`Servidor se ha iniciado en ${PUERTO}`))
---
+++ receptor/src/rutas/producto.js
@@ -0,0 +1,18 @@
+let express = require('express')
+let enrutador = express.Router()
+
+enrutador.get('/producto', (req, res) => {
+
+    /*localhost:3000/producto/producto?nombre=jamon*/
+    if (req.query.nombre) {
+        res.send(`Se ha solicitado un -> ${req.query.nombre}`)
+    }
+
+    res.send('Se ha solicitado un producto')
+})
+
+enrutador.get('/producto/:nombre', (req, res) => {
+    res.send(`Se ha solicitado ${req.params.nombre}`)
+})
+
+module.exports = enrutador
